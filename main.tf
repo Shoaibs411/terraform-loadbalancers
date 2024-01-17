4fdafe620dbc3313378d2511a106c7f6ebd4f3cb
Shoaib <shoaibs411@gmail.com>
2024-01-18 00:51:45 +0530

    Provisioning Load Balancers - TF AutoPush - Creating Security Groups

--- main.tf
+++ main.tf
@@ -2,10 +2,14 @@ module "alb-public"{
   source              = "./vendor/modules/alb"
 
   ENV                       = var.ENV
+  ALB_NAME                  = "roboshop-public"
+  INTERNAL                  = false
 }
 
 module "alb-private"{
   source              = "./vendor/modules/alb"
 
   ENV                       = var.ENV
+  ALB_NAME                  = "roboshop-private"
+  INTERNAL                  = true
 }
