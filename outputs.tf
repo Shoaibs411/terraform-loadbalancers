ea5bdbebb6b66ede88e6d6aa0532de25a5fa69a1
Shoaib <shoaibs411@gmail.com>
2024-01-27 01:18:37 +0530

    TF Autopush - Creating Backend Components

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "PUBLIC_ALB_ARN" {
 output "PRIVATE_ALB_ARN" {
   value = module.alb-private.ALB_ARN
 }
+
+output "PUBLIC_ALB_ADDRESS" {
+  value = module.alb_public.ALB_ADDRESS
+}
+
+output "PRIVATE_ALB_ADDRESS" {
+  value = module.alb_private.ALB_ADDRESS
+}
+
+output "PRIVATE_LISTENER_ARN" {
+  value = module.alb_private.LISTENER_ARN[0]
+}
