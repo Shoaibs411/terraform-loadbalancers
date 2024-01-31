1a1096d35991436b65fd0e4b7cb5f7d3beb82e8e
Shoaib <shoaibs411@gmail.com>
2024-02-01 00:25:15 +0530

    TF Infra AutoPush - Minor updates for visibility

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "alb-public"{
-  source              = "./vendor/modules/alb"
+  source                    = "./vendor/modules/alb"
 
   ENV                       = var.ENV
   ALB_NAME                  = "roboshop-${var.ENV}-public-alb"
@@ -7,7 +7,7 @@ module "alb-public"{
 }
 
 module "alb-private"{
-  source              = "./vendor/modules/alb"
+  source                    = "./vendor/modules/alb"
 
   ENV                       = var.ENV
   ALB_NAME                  = "roboshop-${var.ENV}-private-alb"
